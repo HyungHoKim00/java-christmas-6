1664caf58fb2e69277ee5f9fd0659e0287a5fdb1
HyungHoKim <hkim1109@naver.com>
2023-12-15 20:24:45 +0900

    feat(Events): 이벤트 종류를 관리하는 enum 생성
    이벤트의 할인 정도를 가지는 enum 생성

---
+++ src/main/java/christmas/enums/Events.java
@@ -0,0 +1,21 @@
+package christmas.enums;
+
+import static christmas.enums.Menus.샴페인;
+
+public enum Events {
+    DDAY_EVENT(100),
+    WEEKDAY_EVENT(2_023),
+    WEEKEND_EVENT(2_023),
+    SPECIAL_EVENT(1_000),
+    GIFT_EVENT(샴페인.getPrice());
+
+    private final int discountPrice;
+
+    Events(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+}
