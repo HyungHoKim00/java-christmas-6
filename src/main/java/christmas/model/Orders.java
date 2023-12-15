4ae8a19792e9a4df08e2bc4955feff25e1c0339e
HyungHoKim <hkim1109@naver.com>
2023-12-15 21:11:27 +0900

    feat(Orders): 총주문 금액 계산
    총주문 금액을 계산하는 메서드 CalculateTotalPrice 구현

--- src/main/java/christmas/model/Orders.java
+++ src/main/java/christmas/model/Orders.java
@@ -5,6 +5,7 @@ import christmas.utils.validator.OrdersValidator;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Orders {
     private final Map<Menus, Integer> orders;
@@ -16,5 +17,11 @@ public class Orders {
             String[] menuAndAmount = order.split("-");
             this.orders.put(Menus.getByName(menuAndAmount[0]), Integer.parseInt(menuAndAmount[1]));
         });
+    }
+
+    public int calculateTotalPrice() {
+        AtomicInteger totalPrice = new AtomicInteger(0);
+        orders.forEach((menu, amount) -> totalPrice.addAndGet(menu.getPrice() * amount));
+        return totalPrice.get();
     }
 }
